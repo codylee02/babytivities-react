fb98fa2862250a9d160cb5742ab2d2a77a50777d
Cody Lee <codylee02@gmail.com>
2020-01-18 10:27:38 -0700

    initial commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: process.env.PORT || 8080,
+    // other stuff
+    //API_BASE_URL: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_BASE_URL : "",
+    API_KEY: process.env.REACT_APP_API_KEY
+  };
+  
