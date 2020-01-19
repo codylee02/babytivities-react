907d9cd1156ba6e5ebfa40757511905d57e6dbee
Cody Lee <codylee02@gmail.com>
2020-01-18 17:42:39 -0700

    added STORE data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import STORE from "./STORE.js";
 
 function App() {
-  return <main className="App">{/* content goes here */}</main>;
+  return <main className="App">Initial Deploy:</main>;
 }
 
 export default App;
---
+++ src/STORE.js
@@ -0,0 +1,114 @@
+module.exports = [
+  {
+    title: "Freeze Game",
+    activityType: "Gross Motor",
+    materials: [
+      "Something that plays music (Phone, CD Player, Bluetooth Speaker, etc.)"
+    ],
+    instructions:
+      "Play a song on your device and encourage your child to dance and wiggle and hop and move as much as they can. When you stop the music, say “FREEZE!” and have them stay as still as they can until the music starts playing again.",
+    age: 3
+  },
+  {
+    title: "Ball Ramps",
+    activityType: "Gross Motor",
+    materials: [
+      "Muffin tin",
+      "A few small balls",
+      "A long board or flat surface (cookie sheet, piece of cardboard, etc.)",
+      "3 different colored pieces of construction paper, cut in half the long way",
+      "Tape"
+    ],
+    instructions:
+      "Place balls inside muffin tin. Lean the piece of cardboard (or whatever else you have chosen) against the stool, and tape it if necessary to hold it in place. Fold the sides of a piece of construction paper up, and then tape the bottom of the ramp to the cardboard. Repeat this with two other pieces of construction paper. Then, have your toddler take the balls from the muffin tin and roll them down the ramps and watch them place them down them again and again!",
+    age: 3
+  },
+  {
+    title: "Crocodile Snap! Game",
+    activityType: "Literacy",
+    materials: [
+      "A bag that cannot be seen through (a tote bag, grocery bag, etc.)",
+      "Small, random items from around the house (Tip: if you want to focus on certain topics, include them in bag: Letters, different colored paper, different animals, numbers, anything you want, really!)"
+    ],
+    instructions:
+      "Have your child in front of you with your bag of items beside you. Move your hands like a crocodile and sing: “Crocodile, Crocodile, down by the lake, I’m going to reach my hands in your bag and see what you ate!” Then reach in the bag and pull out an item. Give your child a few seconds to say the object first, and then sound out the object together.",
+    age: 3
+  },
+  {
+    title: "Parking Game",
+    activityType: "Literacy",
+    materials: [
+      "Large piece of paper",
+      "Marker",
+      "Any toy cars, blocks, legos, or other toys you might have laying around the house",
+      "A piece of paper",
+      "Tape",
+      "Scissors"
+    ],
+    instructions:
+      "On the large piece of paper, draw parking spots that are big enough for the cars or other toys you have, and write a different letter or number in each spot. On the smaller piece of paper, write corresponding letters that are small enough to fit on the toys, then cut out the letters and tape them to the toys. Have your child match the toys to the correct letter on the paper, and have fun with it! If they place a toy in the “wrong” spot, ask them if the letters match up and if they can see the same letter anywhere else.",
+    age: 3
+  },
+  {
+    title: "Stickers!",
+    activityType: "Art",
+    materials: ["Stickers", "Lots of spare paper"],
+    instructions:
+      "Allow your child to just stick those stickers you have laying around wherever they want on the paper! Many people tend to only let their children use a few stickers at a time. However, stickers are a great art AND fine motor activity because they challenge children to peel off stickers, which is not always easy to do. As long as they do not stick their stickers on everything in sight, stickers can be a great, no-mess alternative to painting or coloring!",
+    age: 3
+  },
+  {
+    title: "Pom-Pom Painting",
+    activityType: "Art",
+    materials: [
+      "Pom-Poms (or cotton balls, small pieces of sponge)",
+      "Clothespins",
+      "Paper",
+      "Paint",
+      "Bowl for paint"
+    ],
+    instructions:
+      "Attach a few cotton balls to clothespins, and let your child use them as an alternative to paint brushes. As simple as this may sound, this is a great way to introduce your child to different utensils, as it is likely that they have never painted with a clothespin. It also provides them with a different experience because of the way that the pom-poms paint compared to a traditional paintbrush.",
+    age: 3
+  },
+  {
+    title: "Non-Traditional Painting",
+    activityType: "Art",
+    materials: [
+      "Anything you can find that can be used to paint with (some of my favorites are things that can be found in nature- leaves, flowers, twigs, grass, hay, small pieces of mulch, pinecones- but can be anything you would like, such as sponges, qtips, feathers, plastic forks, cut apples or potatoes, old toy cars, yarn cookie cutters, a toothbrush, and much more!)",
+      "Paint",
+      "Bowl for paint",
+      "Paper"
+    ],
+    instructions:
+      "With your alternative paintbrush ready, allow your child to explore the different utensil in whatever way they would like with the paint!",
+    age: 3
+  },
+  {
+    title: "Sensory Table Ideas",
+    activityType: "Sensory",
+    materials: [
+      "Any textured item you can think of (and are okay with not using again)",
+      "Sand, flour, water (cold or warm) pasta (cooked or uncooked), small pebbles, rice, popcorn kernels, dry beans, puffy shaving cream (not the gel kind), animal bedding, bird/ sunflower seeds, potting soil, shredded paper, wheat, water marbles (make sure these are NEVER digested), etc.",
+      "Small toys that can be played with in the items above: Plastic/rubber animals, sand toys, plastic blocks, bath toys, funnels, etc.",
+      "Shallow plastic container",
+      "Lots of towels to put underneath container"
+    ],
+    instructions:
+      "Pour your sensory item into the plastic container, as well as a few toys for your little one to dig and find (make sure not to add too many toys as this can sometimes be overwhelming! Stick to 2-3 larger toys, and 5-6 smaller ones)",
+    age: 3
+  },
+  {
+    title: "Sonsory Bags",
+    activityType: "Sensory",
+    materials: [
+      "Plastic Ziploc bags (the thicker the plastic on the bags, the better to prevent holes) (nail polish remover and a cotton ball will take the Ziploc sign right off the bag)",
+      "Large bottle of cheap hair gel",
+      "Optional: Small objects to be placed in bag (buttons, plastic animals, beats, sequins, glitter, or anything else you can find around the house)",
+      "Duct tape"
+    ],
+    instructions:
+      "Squeeze the bottle of hair gel into the bag, then add the small objects to the bag if you would like to. Seal it and tape up all of the edges of the bag really well. If you would like, you can tape the bag to a hard surface such as a table or window so that your child can’t poke holes in it as easily.",
+    age: 3
+  }
+];
