0fb988e8d86594fffee90ccc763c91233570dbe8
Cody Lee <codylee02@gmail.com>
2020-01-21 12:06:31 -0700

    added drop down

--- src/routes/Activities/Activities.js
+++ src/routes/Activities/Activities.js
@@ -11,6 +11,23 @@ export default function Activities(props) {
   return (
     <div className="Activities">
       <h1>Activities</h1>
+      <div>
+        Type:
+        <select>
+          <option>All activity types</option>
+          <option>Sensory</option>
+          <option>Critical Thinking</option>
+        </select>
+      </div>
+      <div>
+        Age:
+        <select>
+          <option>All ages</option>
+          <option value="0-12m">0 - 12 Months</option>
+          <option value="13-24m">13 - 24 Months</option>
+          <option value="25-36m">25 - 36 Months</option>
+        </select>
+      </div>
       <ul className="Activities__ul">{activities}</ul>
     </div>
   );
