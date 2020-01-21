c5f9ed5b0fbdd7d5a7244413d02c1bbbb602458f
Cody Lee <codylee02@gmail.com>
2020-01-20 21:53:11 -0700

    made list item nice

--- src/components/ActivityListItem/ActivityListItem.js
+++ src/components/ActivityListItem/ActivityListItem.js
@@ -8,8 +8,18 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 export default function ActivityListItem(props) {
   return (
     <li className="ActivityListItem">
-      <Link to={`activities/${props.activity.id}`}>{props.activity.title}</Link>
-      <FontAwesomeIcon icon={faHeart} />
+      <div className="ActivityListItem__title">
+        <Link to={`activities/${props.activity.id}`}>
+          {props.activity.title}
+        </Link>
+        <div className="ActivityListItem__meta">
+          <span> {props.activity.age}</span>{" "}
+          <span>{props.activity.activityType}</span>
+        </div>
+      </div>
+      <div className="ActivityListItem__favorite">
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
     </li>
   );
 }
