b58330dc4a81ac8905eb245681073ef574e029ce
Cody Lee <codylee02@gmail.com>
2020-02-11 18:16:25 -0700

    fixed api url

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   API_BASE_URL:
     process.env.NODE_ENV === "development"
       ? process.env.REACT_APP_API_BASE_URL
-      : "https://immense-atoll-77292.herokuapp.com/api",
+      : "https://babytivities-api.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY
 };
