c8687d70335fc5bbb2f3ae54637ba3b9415ca030
Cody Lee <codylee02@gmail.com>
2020-02-16 20:13:23 -0700

    now knows if user is logged in to display in/out

--- src/components/LandingPageNav/LandingPageNav.js
+++ src/components/LandingPageNav/LandingPageNav.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import TokenService from "../../services/token-service";
 
 import "./LandingPageNav.css";
 
 export default function LandingPageNav() {
+  let logoutOrLogin = TokenService.hasAuthToken() ? "Logout" : "Login";
+  let logoutOrLoginLink = TokenService.hasAuthToken() ? "/" : "/login";
   return (
     <nav role="navigation" className="LandingPageNav">
       {/* <Link to="/register">Register</Link> */}
       <Link to="/activities">Activities</Link>
-      <Link to="/login">Login</Link>
+      <Link
+        to={`${logoutOrLoginLink}`}
+        onClick={() => this.handleLogoutClick()}
+      >
+        {logoutOrLogin}
+      </Link>
     </nav>
   );
 }
